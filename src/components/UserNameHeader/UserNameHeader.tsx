d5430e873a37af8d49a136e2030af9350304146d
MaryZaiko <604926@gmail.com>
2022-06-29 12:19:13 +0300

    fix UserNameHeader

--- src/components/UserNameHeader/UserNameHeader.tsx
+++ src/components/UserNameHeader/UserNameHeader.tsx
@@ -1,14 +1,24 @@
 import React, { FC } from "react";
 import "./UserNameHeader.css";
 import Button from "../Button";
+import UserSVG from "../assets/UserSVG";
 
 const UserNameHeader = () => {
+  let isLogin = true;
   return (
     <div className="userNameHeaderContainer userNameHeaderContainerDark">
       {/* класс зависит от темы */}
-
-      <Button btnContent={"AL"} className="btnUserNameHeader" />
-      <p>Artem Lapitsky</p>
+      {isLogin ? (
+        <div className="userNameHeaderContent">
+          <Button btnContent={"AL"} className="btnUserNameHeader" />
+          <p>Artem Lapitsky</p>
+        </div>
+      ) : (
+        <div className="userNameHeaderContent">
+          <Button btnContent={<UserSVG/>} className="btnUserNameHeader" />
+          <p>Sing in</p>
+        </div>
+      )}
     </div>
   );
 };
